486928f4f21ac8af14cc90ec0aa746b4bcc4d2bd
Rak.Dominik <209443@edu.p.lodz.pl>
2019-03-06 18:47:54 +0100

    full functionality added

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,16 +1,20 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
         if (value == null) {
-            return new ArrayList<>();
+            return Collections.EMPTY_LIST;
         }
         List<String> tailsList = new ArrayList<>(value.length() + 1);
-        tailsList.add(0, value);
+
+        for (int i = 0; i <= value.length(); i++) {
+            tailsList.add(i, value.substring(i));
+        }
         return tailsList;
     }
 
